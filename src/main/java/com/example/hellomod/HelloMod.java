a53a9a10cba257baa59424a1538245d8ce038d08
liuzeen1234 <liuzeen1234@qq.com>
2026-08-17 22:04:19 +0800

    Initial commit: Fabric mod project setup

---
+++ src/main/java/com/example/hellomod/HelloMod.java
@@ -0,0 +1,23 @@
+package com.example.hellomod;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
+import net.minecraft.text.Text;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HelloMod implements ModInitializer {
+
+    public static final String MOD_ID = "hello-mod";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+        LOGGER.info("Hello Mod initialized!");
+
+        // 当玩家加入世界时，在聊天框输出 "hello"
+        ServerPlayConnectionEvents.JOIN.register((handler, sender, server) -> {
+            handler.getPlayer().sendMessage(Text.literal("hello"), false);
+        });
+    }
+}
