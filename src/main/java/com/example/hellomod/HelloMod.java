2563ee02b45575e07f97b6132c12335bd79f7037
liuzeen1234 <liuzeen1234@qq.com>
2026-08-19 14:04:40 +0800

    chore: Remove unused hello message on player join
    - Remove unused import for Text class
    - Remove player join event listener that sent "hello" message to chat
    - Simplify mod initialization by removing unnecessary debug output

--- src/main/java/com/example/hellomod/HelloMod.java
+++ src/main/java/com/example/hellomod/HelloMod.java
@@ -6,7 +6,6 @@ import com.example.hellomod.enchantment.InfinityCooldownManager;
 import com.example.hellomod.enchantment.ModEnchantments;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
-import net.minecraft.text.Text;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -30,11 +29,6 @@ public class HelloMod implements ModInitializer {
 
         // 注册无限附魔冷却管理器（tick事件）
         InfinityCooldownManager.register();
-
-        // 当玩家加入世界时，在聊天框输出 "hello"
-        ServerPlayConnectionEvents.JOIN.register((handler, sender, server) -> {
-            handler.getPlayer().sendMessage(Text.literal("hello"), false);
-        });
 
         // 玩家断开连接时清理无限冷却数据
         ServerPlayConnectionEvents.DISCONNECT.register((handler, server) -> {
